df1b6bddbb6c255d3200d3bca1c1c73a5607a478
Niel Buys <nbuys@ncomp.co.za>
2025-01-23 15:50:27 +0200

    Version Up

--- libraries/src/Version.php
+++ libraries/src/Version.php
@@ -49,7 +49,7 @@ final class Version
 	 * @var    integer
 	 * @since  3.8.0
 	 */
-	const PATCH_VERSION = 4;
+	const PATCH_VERSION = 5;
 
 	/**
 	 * Extra release version info.
@@ -111,7 +111,7 @@ final class Version
 	 * @var    string
 	 * @since  3.5
 	 */
-	const RELDATE = '12-November-2024';
+	const RELDATE = '23 January 2025';
 
 	/**
 	 * Release time.
